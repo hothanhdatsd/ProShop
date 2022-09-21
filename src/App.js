f112aab6d46e7f8b7b21032098be651cdce63fe1
ThanhDattt <93121654+hothanhdatsd@users.noreply.github.com>
2022-09-22 00:09:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import "./bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>} exact  />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart" element={<HomeScreen />} />
+            <Route path="/login" element={<HomeScreen />} />
+
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
